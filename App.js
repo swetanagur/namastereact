03a4e77b43097bac35b14ba2f28030729b376388
swetanagur <swetanagur@gmail.com>
2024-03-04 23:17:36 -0500

    episode 1

---
+++ App.js
@@ -0,0 +1,8 @@
+const parent = React.createElement(
+    "div",{id:"parent"},
+    React.createElement("div",{id:"child"},
+    [React.createElement("h1",{},"Hello h1"),React.createElement("h2",{},"Hello h2")]));
+console.log(ReactDOM);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
+root.render(parent);
