32852a05b8dc64029529fcb8bd3436f816281840
swetanagur <swetanagur@gmail.com>
2024-03-06 11:43:25 -0500

    Episode 2 ignition

--- episode1/App.js
+++ episode1/App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
     "div",{id:"parent"},
     React.createElement("div",{id:"child"},
-    [React.createElement("h1",{},"Hello h1"),React.createElement("h2",{},"Hello h2")]));
+    [React.createElement("h1",{},"Hello h1 🚀"),React.createElement("h2",{},"Hello h2 ")]));
 console.log(ReactDOM);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 console.log(root);
